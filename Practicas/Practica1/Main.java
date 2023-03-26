a3739625d3f1b557563a8f1952643a12aeb2ca2a
Valen <96664741+ValenUNLP@users.noreply.github.com>
2023-03-26 18:48:40 -0300

    Añadiendo Ej2 de la Practica 1

--- Practicas/Practica1/Main.java
+++ Practicas/Practica1/Main.java
@@ -1,15 +1,26 @@
 package Practica1;
 
+import java.util.Scanner;
+
 public class P1 {
 
 	public static void main(String[] args) {
 		listarNumeros prueba1 = new listarNumeros(3,10);
+		multiplos prueba2;
+		Scanner s = new Scanner(System.in);
+
 		
 		//Ej1
 		//prueba.listarConFor();
 		//prueba.listarConWhile();
 		//prueba.listarSinEstructuras();
-
+		
+		//Ej2
+		
+		int n = s.nextInt();
+		prueba2 = new multiplos(n);
+		//prueba2.arrMultiplos();
+		//System.out.println(prueba2.toString());
 		
 	}
 }
