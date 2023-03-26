edb5f58419e0aa0a3e1a0f1b3b3444462c7bddf2
Valen <96664741+ValenUNLP@users.noreply.github.com>
2023-03-26 19:30:51 -0300

    Añadiendo Ej3 de la Practica 1

---
+++ Practicas/Practica1/estudiante.java
@@ -0,0 +1,60 @@
+package Practica1B;
+
+public class estudiante {
+
+	String nombre;
+	String apellido;
+	String comision;
+	String email;
+	String direccion;
+	
+	public estudiante(String nombre, String apellido, String comision, String email, String direccion) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.comision = comision;
+		this.email = email;
+		this.direccion = direccion;
+	}
+	
+	public estudiante() {
+		
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getComision() {
+		return comision;
+	}
+	public void setComision(String comision) {
+		this.comision = comision;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	public String tusDatos(){
+		String aux;
+		aux = this.getApellido() + " " + this.getNombre() + " " + this.getComision() + " " + this.getDireccion() + " " + this.getEmail();
+		return aux;
+	}
+
+	
+}
