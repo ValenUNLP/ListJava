91ebab2f951d3f71a45b24a714b5af51fbf9122f
Valen <96664741+ValenUNLP@users.noreply.github.com>
2023-03-26 18:10:10 -0300

    Agregando Main de la Practica 1

---
+++ Practicas/Practica1/Main.java
@@ -0,0 +1,15 @@
+package Practica1;
+
+public class P1 {
+
+	public static void main(String[] args) {
+		listarNumeros prueba1 = new listarNumeros(3,10);
+		
+		//Ej1
+		//prueba.listarConFor();
+		//prueba.listarConWhile();
+		//prueba.listarSinEstructuras();
+
+		
+	}
+}
