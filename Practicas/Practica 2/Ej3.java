ffed12dab7deb6d4613ab78001cd8a507253e81f
Valen <96664741+ValenUNLP@users.noreply.github.com>
2023-03-29 19:43:28 -0300

    Create Ej3.java

---
+++ Practicas/Practica 2/Ej3.java
@@ -0,0 +1,38 @@
+package Practica2;
+
+import tp02.ejercicio3.ColaGenerica;
+import tp02.ejercicio3.PilaGenerica;
+
+public class Ej3 {
+	
+	public static void main(String[] args) {
+		//PilaGenerica<Integer> Pila = new PilaGenerica<>();
+		ColaGenerica<Integer> Cola = new ColaGenerica<>();
+		//System.out.println(Pila.esVacia());
+		
+		//Pila.apilar(10);
+		//Pila.apilar(20);
+		//Pila.apilar(130);
+		
+		//System.out.println(Pila.esVacia());
+		
+		//System.out.println(Pila.tope());
+		//System.out.println(Pila.toString());
+		//System.out.println(Pila.desapilar());
+		//System.out.println(Pila.toString());
+		
+		//System.out.println(Cola.esVacia());
+		
+		//Cola.encolar(10);
+		//Cola.encolar(20);
+		//Cola.encolar(130);
+		
+		//System.out.println(Cola.esVacia());
+		
+		//System.out.println(Cola.tope());
+		//System.out.println(Cola.toString());
+		//System.out.println(Cola.desencolar());
+		//System.out.println(Cola.toString());
+		
+	}
+}
